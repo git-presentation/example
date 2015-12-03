c50e214075ef2db89c420069fcd9cd2ff35ec04e
Sven Baars <s.baars@rug.nl>
2015-12-03 14:13:30 +0100

    Fix relative paths

--- scripts/test/bad-merge-example.sh
+++ scripts/test/bad-merge-example.sh
@@ -4,7 +4,7 @@ cd local-repository
 git push
 git checkout feature2
 git pull --no-edit origin master
-sh "$dir/../../random-commit.sh"
+sh "$../dir/../random-commit.sh"
 git checkout master
 git merge --no-ff --no-edit feature2
 git log --oneline --graph --all --decorate
--- scripts/test/log-example.sh
+++ scripts/test/log-example.sh
@@ -1,13 +1,13 @@
 dir="$(dirname "$0")"
 sh "$dir/../init-repos.sh"
 cd local-repository
-sh "$dir/../../random-commit.sh"
-sh "$dir/../../random-commit.sh"
-sh "$dir/../../random-commit.sh"
-sh "$dir/../../random-commit.sh"
-sh "$dir/../../random-commit.sh"
-sh "$dir/../../random-commit.sh"
-sh "$dir/../../random-commit.sh"
-sh "$dir/../../random-commit.sh"
+sh "../$dir/../random-commit.sh"
+sh "../$dir/../random-commit.sh"
+sh "../$dir/../random-commit.sh"
+sh "../$dir/../random-commit.sh"
+sh "../$dir/../random-commit.sh"
+sh "../$dir/../random-commit.sh"
+sh "../$dir/../random-commit.sh"
+sh "../$dir/../random-commit.sh"
 git log
 git log --oneline --graph --all --decorate
